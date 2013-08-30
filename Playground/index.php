227455a111df7a2d41e5d3cf7df3fbe070589a9e
Exclure <zero321@client163-132.wireless.newpaltz.edu>
2013-08-30 16:50:57 -0400

    background color added

--- Playground/index.php
+++ Playground/index.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+    <style>
+    	div { background-color: AACCCC; }
+    </style>
   </head>
   <body>
     <h1>Hello this is the Playground</h1>
