3a3a6e51d0dc5de86f8c98527f027929cd55da28
Exclure <zero321@client163-132.wireless.newpaltz.edu>
2013-09-09 16:19:48 -0400

    added to nav bar

--- Playground/contact.php
+++ Playground/contact.php
@@ -5,13 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
-    <style>
-    	div { background-color: #AACCCC; }
-    </style>
   </head>
   <body>
-    <h1>Hello World!</h1>
-    <h2>Welcome class of 2013 to Web Server Programing</h2>
     <nav class="navbar navbar-inverse" role="navigation">
   <!-- Brand and toggle get grouped for better mobile display -->
   <div class="navbar-header">
@@ -27,8 +22,9 @@
   <!-- Collect the nav links, forms, and other content for toggling -->
   <div class="collapse navbar-collapse navbar-ex1-collapse">
     <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Link</a></li>
-      <li><a href="#">Link</a></li>
+      <li class="active"><a href="#">Home</a></li>
+      <li><a href="#">Contact</a></li>
+      <li><a href="#">Links</a></li>
       <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
         <ul class="dropdown-menu">
@@ -60,7 +56,8 @@
     </ul>
   </div><!-- /.navbar-collapse -->
 </nav>
-    
+     <h1>Hello World!</h1>
+    <h2>Welcome class of 2013 to Web Server Programing</h2>
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
   </body>
