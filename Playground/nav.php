df83eda96220f18cd241937e4e89ad76e3778d02
Exclure <zero321@client163-132.wireless.newpaltz.edu>
2013-09-12 15:52:05 -0400

    added nav file

---
+++ Playground/nav.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Playground</a>
+				</div>
+
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active">
+							<a href="index.php">Home</a>
+						</li>
+						<li>
+							<a href="contact.php">Contact</a>
+						</li>
+						<li>
+							<a href="links.php">Links</a>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li><a href="#">Action</a></li>
+								<li><a href="#">Another action</a></li>
+								<li><a href="#">Something else here</a></li>
+								<li><a href="#">Separated link</a></li>
+								<li><a href="#">One more separated link</a></li>
+							</ul>
+						</li>
+					</ul>
+					<p class="navbar-text pull-right"> Signed in as <a href="#" class="navbar-link">Lynton Merriwether</a></p>
+				</div><!-- /.navbar-collapse -->
+			</div>
+		</nav>
