62478741110e8dd8b7f27b6b96ab4428b9785921
Exclure <zero321@client163-132.wireless.newpaltz.edu>
2013-09-12 14:41:20 -0400

    edited home page

--- Playground/index.php
+++ Playground/index.php
@@ -10,54 +10,54 @@
 		<nav class="navbar navbar-inverse" role="navigation">
 			<!-- Brand and toggle get grouped for better mobile display -->
 			<div class="container">
-			<div class="navbar-header">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</button>
-				<a class="navbar-brand" href="#">Playground</a>
-			</div>
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Playground</a>
+				</div>
 
-			<!-- Collect the nav links, forms, and other content for toggling -->
-			<div class="collapse navbar-collapse navbar-ex1-collapse">
-				<ul class="nav navbar-nav">
-					<li class="active">
-						<a href="index.php">Home</a>
-					</li>
-					<li>
-						<a href="contact.php">Contact</a>
-					</li>
-					<li>
-						<a href="links.php">Links</a>
-					</li>
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-						<ul class="dropdown-menu">
-							<li>
-								<a href="#">Action</a>
-							</li>
-							<li>
-								<a href="#">Another action</a>
-							</li>
-							<li>
-								<a href="#">Something else here</a>
-							</li>
-							<li>
-								<a href="#">Separated link</a>
-							</li>
-							<li>
-								<a href="#">One more separated link</a>
-							</li>
-						</ul>
-					</li>
-				</ul>
-				<p class="navbar-text pull-right">
-					Signed in as <a href="#" class="navbar-link">Lynton Merriwether</a>
-				</p>
-			</div><!-- /.navbar-collapse -->
-		</div>
+				<!-- Collect the nav links, forms, and other content for toggling -->
+				<div class="collapse navbar-collapse navbar-ex1-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active">
+							<a href="index.php">Home</a>
+						</li>
+						<li>
+							<a href="contact.php">Contact</a>
+						</li>
+						<li>
+							<a href="links.php">Links</a>
+						</li>
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
+							<ul class="dropdown-menu">
+								<li>
+									<a href="#">Action</a>
+								</li>
+								<li>
+									<a href="#">Another action</a>
+								</li>
+								<li>
+									<a href="#">Something else here</a>
+								</li>
+								<li>
+									<a href="#">Separated link</a>
+								</li>
+								<li>
+									<a href="#">One more separated link</a>
+								</li>
+							</ul>
+						</li>
+					</ul>
+					<p class="navbar-text pull-right">
+						Signed in as <a href="#" class="navbar-link">Lynton Merriwether</a>
+					</p>
+				</div><!-- /.navbar-collapse -->
+			</div>
 		</nav>
 		<div class="container">
 			<div class="jumbotron">
@@ -68,25 +68,39 @@
 				<a class="btn btn-lg btn-success">Learn More</a>
 
 			</div>
-		<div class="row">
-		  <div class="col-xs-6 col-md-4">
-		  	<h2>Heading</h2>
-		  	<p><a class="btn btn-primary" href="#">View details »</a></p>
-		  </div>
-		  <div class="col-xs-6 col-md-4">
-		  	<h2>Important Stuff</h2>
-		  	<p>Check out the August issue of Ludique Magazine The Mobile Photography Magazine</p><p><a class="btn btn-primary" href="http://ludiquemag.com/august2013.html">Check It Out »</a></p>
+			<div class="row">
+				<div class="col-xs-6 col-md-4">
+					<h2>Heading</h2>
+					<p>
+						Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+					</p>
+					<p>
+						<a class="btn btn-primary" href="#">View details »</a>
+					</p>
+				</div>
+				<div class="col-xs-6 col-md-4">
+					<h2>Important Stuff</h2>
+					<p>
+						Check out the August issue of Ludique Magazine The Mobile Photography Magazine
+					</p>
+					<p>
+						<a class="btn btn-primary" href="http://ludiquemag.com/august2013.html">Check It Out »</a>
+					</p>
+				</div>
+				<div class="col-xs-6 col-md-4">
+					<h2>Heading</h2>
+					<p>
+						Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+					</p>
+					<p>
+						<a class="btn btn-primary" href="#">View details »</a>
+					</p>
+				</div>
 			</div>
-		  <div class="col-xs-6 col-md-4">
-		  	<h2>Heading</h2>
-		  	<p>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.</p>
-		  	<p><a class="btn btn-primary" href="#">View details »</a></p>
-		  	</div>
-		</div>
 
-		<!-- Columns are always 50% wide, on mobile and desktop -->
-		
-	</div>
+			<!-- Columns are always 50% wide, on mobile and desktop -->
+
+		</div>
 		</div>
 		<script src="http://code.jquery.com/jquery.js"></script>
 		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
